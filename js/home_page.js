548a75eeaf73dc24a662ee1fcac836329d3ae301
Hibiki93 <yue-555@hotmail.com>
2018-08-17 18:26:17 +0800

    first_commit

---
+++ js/home_page.js
@@ -0,0 +1,10 @@
+// window.setTimeout(function() {
+//     // alert('test');
+
+// }, 3000);
+
+// setTimeout(function() {
+//     $('html, body').animate({
+//         scrollTop: 0
+//     }, 2000);
+// }, 4000);
